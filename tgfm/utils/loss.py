e3a9b2e35faf9f4227553c779294d6c314f79dbf
sebastian9991 <sebastian.sabry1@gmail.com>
2026-04-22 14:33:27 -0400

    logging

--- tgfm/utils/loss.py
+++ tgfm/utils/loss.py
@@ -1,3 +1,5 @@
+import logging
+
 import torch
 import torch.nn as nn
 
@@ -6,8 +8,10 @@ def compute_mlm_loss(
     mlm_logits: torch.Tensor, input_ids: torch.Tensor, masked_input_ids: torch.Tensor
 ) -> torch.Tensor:
     mask = input_ids != masked_input_ids
+    logging.info(f'mask: {mask}')
 
     target_ids = input_ids[mask]
+    logging.info(f'target_ids: {target_ids}')
 
     assert target_ids.shape[0] == mlm_logits.shape[0], (
         f'Shape mismatch: Logits {mlm_logits.shape}, Targets {target_ids.shape}. '
