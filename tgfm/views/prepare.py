ac0f2e75b7f22d9ea1be9329781c63c1a4097015
sebastian9991 <sebastian.sabry1@gmail.com>
2026-06-08 18:01:36 -0400

    trying object version of transform undirected

--- tgfm/views/prepare.py
+++ tgfm/views/prepare.py
@@ -11,11 +11,12 @@ from dataclasses import dataclass, field
 from typing import List, Optional
 
 import torch
+import torch_geometric.transforms as T
 from torch import Tensor
 from torch_geometric.data import Data
 from torch_geometric.loader import ClusterData  # METIS wrapper
 from torch_geometric.transforms import AddRandomWalkPE
-from torch_geometric.utils import k_hop_subgraph, subgraph, to_undirected
+from torch_geometric.utils import k_hop_subgraph, subgraph
 
 
 @dataclass
@@ -91,12 +92,12 @@ def build_local_views_metis(
             num_nodes=N,
         )
 
-        sub_ei = to_undirected(edge_index=sub_ei)
         view = Data(
             x=data.x[sub_nodes],
             edge_index=sub_ei,
             pe=rwse[sub_nodes],
         )
+        view = T.ToUndirected()(view)
         if se is not None:
             view.se = se[sub_nodes]
         if getattr(data, 'edge_attr', None) is not None:
@@ -140,12 +141,12 @@ def build_global_views(
             raise ValueError(f'Unknown global view strategy: {strategy}')
 
         sub_ei, _ = subgraph(nodes, data.edge_index, relabel_nodes=True, num_nodes=N)
-        sub_ei = to_undirected(edge_index=sub_ei)
         view = Data(
             x=data.x[nodes],
             edge_index=sub_ei,
             pe=rwse[nodes],
         )
+        view = T.ToUndirected()(view)
         if se is not None:
             view.se = se[nodes]
         views.append(view)
