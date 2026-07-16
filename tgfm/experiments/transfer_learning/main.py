b34d94f26e7684701843efb3aa01ed3f7b719f77
sebastian9991 <sebastian.sabry1@gmail.com>
2026-07-15 22:00:35 -0400

    changing static folder locations

--- tgfm/experiments/transfer_learning/main.py
+++ tgfm/experiments/transfer_learning/main.py
@@ -318,12 +318,11 @@ def pretrain(
 
     # prepare pretraining data
     dataset: Optional[Union[LazyGraphDataset, List[Any]]] = None
+    # TODO: dyanmic folder locations
     if 'papers100M' == data_args.data_name:
-        data_dir = '/localscratch/GFM_data/papers100M/subgraphs'
+        data_dir = '$SCRATCH/GSTBench/subgraph/'
         graph_name = 'papers100M'
-        feature_path = (
-            '/localscratch/GFM_data/papers100M/emb/sbert_embeddings_con_split.npy'
-        )
+        feature_path = '$SCRATCH/GSTBench/emb/sbert_embeddings_con_split.npy'
         graph_list, n_node_list, n_edge_list = load_subgraphs(data_dir, graph_name)
         if data_args.pretrain_data_ids[0] != -1:
             graph_list = [graph_list[id] for id in data_args.pretrain_data_ids]
