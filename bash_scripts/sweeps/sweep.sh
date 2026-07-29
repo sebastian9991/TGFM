16fd731a5abb68d35fb0233003135730d5550c70
sebastian9991 <sebastian.sabry1@gmail.com>
2026-07-29 18:37:30 -0400

    updating sweep.sh

--- bash_scripts/sweeps/sweep.sh
+++ bash_scripts/sweeps/sweep.sh
@@ -3,4 +3,4 @@ set -e
 uv run --group graphclip torchrun --standalone \
     --nproc_per_node=${SLURM_GPUS_ON_NODE:-4} \
     tgfm/experiments/leGTjepa/main.py \
-    --config-file configs/legtjepa/bert.yaml
+    --config-file configs/legtjepa/sweep/base.yaml
